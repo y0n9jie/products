ee0010e39a490a833795748cc8f11ad46f650900
River Chen <j0n9jie.c@gmail.com>
2023-03-23 15:12:05 +0800

    add read file

--- products.py
+++ products.py
@@ -1,4 +1,20 @@
+# 讀取檔案
+import os
+
 products = []
+if os.path.isfile('products.csv'):
+	print('找到檔案了！')
+	with open('products.csv', 'r', encoding='utf-8') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue
+			name, price = line.strip().split(',')
+			products.append([name, price])
+	print(products)
+else:
+	print('找不到檔案……')
+
+# 讓使用者輸入
 while True:
 	name = input('請輸入消費項目：')
 	if name == 'q':
@@ -8,11 +24,12 @@ while True:
 	products.append([name, price])
 print(products)
 
+# 印出消費紀錄
 for p in products:
 	print(p[0], '的金額是', p[1])
 
+# 寫入檔案
 with open('products.csv', 'w', encoding='utf-8') as f:
 	f.write('商品,價格\n')
 	for p in products:
 		f.write(p[0] + ',' + str(p[1]) + '\n')
-
